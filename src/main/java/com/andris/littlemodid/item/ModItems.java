c8e9bdca4c2090ad114c02ac0627728c16e9f4e9
BlackfoxOo <olahimre24@gmail.com>
2023-08-20 18:19:50 +0200

    Added Items: -Iron Dust -Gold Dust -Diamond Dust -Wrought Iron Dust -Netherite Dust -Diawronite Alloy Dust -Uranium Dust -Rainbow Dye -Mortar

--- src/main/java/com/andris/littlemodid/item/ModItems.java
+++ src/main/java/com/andris/littlemodid/item/ModItems.java
@@ -14,10 +14,27 @@ import net.minecraft.util.Identifier;
 public class ModItems {
 
     public static final Item DIAWRONITE_ALLOY_INGOT = registerItem("diawronite_alloy_ingot", new Item(new FabricItemSettings()));
-
+    public static final Item GOLD_DUST = registerItem("gold_dust", new Item(new FabricItemSettings()));
+    public static final Item DIAMOND_DUST = registerItem("diamond_dust", new Item(new FabricItemSettings()));
+    public static final Item NETHERITE_DUST = registerItem("netherite_dust", new Item(new FabricItemSettings()));
+    public static final Item IRON_DUST = registerItem("iron_dust", new Item(new FabricItemSettings()));
+    public static final Item WROUGHT_IRON_DUST = registerItem("wrought_iron_dust", new Item(new FabricItemSettings()));
+    public static final Item DIAWRONITE_ALLOY_DUST = registerItem("diawronite_alloy_dust", new Item(new FabricItemSettings()));
+    public static final Item MORTAR = registerItem("mortar", new Item(new FabricItemSettings()));
+    public static final Item URANIUM_DUST = registerItem("uranium_dust", new Item(new FabricItemSettings()));
+    public static final Item RAINBOW_DYE = registerItem("rainbow_dye", new Item(new FabricItemSettings()));
     private static void addItemsToIngredientsItemGroup(FabricItemGroupEntries entries) {
         entries.add(DIAWRONITE_ALLOY_INGOT);
         entries.add(ModBlocks.DIAWRONITE_BLOCK);
+        entries.add(GOLD_DUST);
+        entries.add(DIAMOND_DUST);
+        entries.add(NETHERITE_DUST);
+        entries.add(IRON_DUST);
+        entries.add(WROUGHT_IRON_DUST);
+        entries.add(MORTAR);
+        entries.add(URANIUM_DUST);
+        entries.add(DIAWRONITE_ALLOY_DUST);
+        entries.add(RAINBOW_DYE);
     }
 
     private static Item registerItem(String name, Item item) {
