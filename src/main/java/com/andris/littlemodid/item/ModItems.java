9d37a0d22f451703985cc8626fa572d3c5027d1b
makeitworkAndris <andrisbarath5@gmail.com>
2023-08-20 18:49:06 +0200

    Added Diawronite pickaxe.

--- src/main/java/com/andris/littlemodid/item/ModItems.java
+++ src/main/java/com/andris/littlemodid/item/ModItems.java
@@ -2,6 +2,7 @@ package com.andris.littlemodid.item;
 
 import com.andris.littlemodid.LittleModMain;
 import com.andris.littlemodid.block.ModBlocks;
+import com.andris.littlemodid.item.custom.ModPickaxeItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
@@ -14,10 +15,14 @@ import net.minecraft.util.Identifier;
 public class ModItems {
 
     public static final Item DIAWRONITE_ALLOY_INGOT = registerItem("diawronite_alloy_ingot", new Item(new FabricItemSettings()));
+    public static final Item DIAWRONITE_PICKAXE = registerItem("diawronite_pickaxe",
+            new ModPickaxeItem(ModToolMaterials.DIAWRONITE,1,2f, new FabricItemSettings()));
+
 
     private static void addItemsToIngredientsItemGroup(FabricItemGroupEntries entries) {
         entries.add(DIAWRONITE_ALLOY_INGOT);
         entries.add(ModBlocks.DIAWRONITE_BLOCK);
+        entries.add(DIAWRONITE_PICKAXE);
     }
 
     private static Item registerItem(String name, Item item) {
