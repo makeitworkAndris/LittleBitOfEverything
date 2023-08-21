994c183338f871a69d694c3098d4877f1fa266ef
BlackfoxOo <olahimre24@gmail.com>
2023-08-21 18:20:12 +0200

    Fixed Acid Balade Music Disc

--- src/main/java/com/andris/littlemodid/LittleModDataGenerator.java
+++ src/main/java/com/andris/littlemodid/LittleModDataGenerator.java
@@ -1,11 +1,17 @@
 package com.andris.littlemodid;
 
+import com.andris.littlemodid.data.ModModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.client.ModelProvider;
+
 
 public class LittleModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
 	}
 }
