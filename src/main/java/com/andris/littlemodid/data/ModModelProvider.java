b2497c44b7ed021f520a448a6f8ceefed4073597
BlackfoxOo <olahimre24@gmail.com>
2023-08-21 12:18:38 +0200

    Added Music Disc Item [Progress: 90%]

--- src/main/java/com/andris/littlemodid/data/ModModelProvider.java
+++ src/main/java/com/andris/littlemodid/data/ModModelProvider.java
@@ -3,11 +3,19 @@ package com.andris.littlemodid.data;
 import com.andris.littlemodid.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
 import net.minecraft.data.client.Models;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) { super(output);}
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+
+    }
+
+    @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
         itemModelGenerator.register(ModItems.MUSIC_DISC_ACID_BALADE, Models.GENERATED);
     }
